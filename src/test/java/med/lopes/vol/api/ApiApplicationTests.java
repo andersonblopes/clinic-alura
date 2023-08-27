be0c7cb650a54dc3275908e8496393e454ffcd09
Anderson Lopes <>
2023-08-27 19:55:42 +0200

    Initial commit

---
+++ src/test/java/med/lopes/vol/api/ApiApplicationTests.java
@@ -0,0 +1,15 @@
+package med.lopes.vol.api;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertEquals(1,2-1);
+	}
+
+}
