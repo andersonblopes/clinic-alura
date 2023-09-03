cccd33e1c7e0e7fd7d564e3fb1139371d8843d01
Anderson Lopes <>
2023-09-03 19:30:10 +0200

    Includes doctor table migration

---
+++ src/main/resources/db/migration/V20230903__Create_doctor_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS doctor(
+    id serial PRIMARY KEY,
+    name text NOT NULL UNIQUE,
+    email text NOT NULL UNIQUE,
+    crm text NOT NULL UNIQUE,
+    specialty text NOT NULL,
+    phone text,
+    address_type text,
+    neighborhood text,
+    postal_code text,
+    city text,
+    state text,
+    number text,
+    notes text
+)
