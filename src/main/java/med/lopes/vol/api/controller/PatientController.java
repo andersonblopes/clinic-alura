a3e2441a384c0bc2e0d55758036ff86a2b6b5b8b
Anderson Lopes <>
2023-09-03 23:40:20 +0200

    Improves patient controller

--- src/main/java/med/lopes/vol/api/controller/PatientController.java
+++ src/main/java/med/lopes/vol/api/controller/PatientController.java
@@ -1,8 +1,12 @@
 package med.lopes.vol.api.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import med.lopes.vol.api.model.PatientModel;
 import med.lopes.vol.api.model.input.PatientInput;
+import med.lopes.vol.api.service.PatientService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
  * The type Patient controller.
  */
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/patients")
 public class PatientController {
 
+    private final PatientService patientService;
+
     /**
-     * Create.
+     * Create response entity.
      *
      * @param input the input
+     * @return the response entity
      */
     @PostMapping
-    public void create(@RequestBody @Valid PatientInput input) {
-        log.info(String.valueOf(input));
+    public ResponseEntity<PatientModel> create(@RequestBody @Valid PatientInput input) {
+        return ResponseEntity.ok().body(patientService.create(input));
     }
 
 }
--- src/main/java/med/lopes/vol/api/domain/entities/Patient.java
+++ src/main/java/med/lopes/vol/api/domain/entities/Patient.java
@@ -1,8 +1,22 @@
 package med.lopes.vol.api.domain.entities;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+/**
+ * The type Patient.
+ */
 @Getter
 @Setter
 @Builder
