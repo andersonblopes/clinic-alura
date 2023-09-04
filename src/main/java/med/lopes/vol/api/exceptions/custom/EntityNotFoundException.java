e2c0d693d22da592fee6b7d122f35acd934fd4f7
Anderson Lopes <>
2023-09-04 14:39:56 +0200

    Includes custom exception

---
+++ src/main/java/med/lopes/vol/api/exceptions/custom/EntityNotFoundException.java
@@ -0,0 +1,16 @@
+package med.lopes.vol.api.exceptions.custom;
+
+/**
+ * The type Entity not found exception.
+ */
+public class EntityNotFoundException extends RuntimeException {
+
+    /**
+     * Instantiates a new Entity not found exception.
+     *
+     * @param message the message
+     */
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
