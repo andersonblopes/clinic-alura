bf5b628d3ebe58a426d336a3413229458c5e5376
Anderson Lopes <>
2023-09-09 15:33:16 +0200

    Includes new attribute

--- src/main/java/med/lopes/vol/api/domain/entities/Doctor.java
+++ src/main/java/med/lopes/vol/api/domain/entities/Doctor.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -52,4 +53,12 @@ public class Doctor {
 
     @Embedded
     private Address address;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    @PrePersist
+    private void setDefaultValues() {
+        this.active = Boolean.TRUE;
+    }
 }
