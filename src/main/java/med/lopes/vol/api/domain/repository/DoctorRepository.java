03a872661a837129c466c0ca2260e9587c377def
Anderson Lopes <>
2023-09-09 15:33:54 +0200

    Implements new query using spring data name convention

--- src/main/java/med/lopes/vol/api/domain/repository/DoctorRepository.java
+++ src/main/java/med/lopes/vol/api/domain/repository/DoctorRepository.java
@@ -1,10 +1,14 @@
 package med.lopes.vol.api.domain.repository;
 
 import med.lopes.vol.api.domain.entities.Doctor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * The interface Doctor repository.
  */
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+    Page<Doctor> findByActiveTrue(Pageable pageable);
 }
