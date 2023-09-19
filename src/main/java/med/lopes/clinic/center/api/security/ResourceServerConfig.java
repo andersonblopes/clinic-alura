06468e0f566e7dd5a946f95e99ca04ff85ddcc50
Anderson Lopes <>
2023-09-19 18:04:42 +0200

    Removes deprecated implementation

--- src/main/java/med/lopes/clinic/center/api/security/ResourceServerConfig.java
+++ src/main/java/med/lopes/clinic/center/api/security/ResourceServerConfig.java
@@ -1,20 +1,26 @@
 package med.lopes.clinic.center.api.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/oauth2/**").authenticated()
+                .and()
+                .csrf().disable()
+                .cors()
                 .and()
                 .oauth2ResourceServer().opaqueToken();
+
+        return http.build();
     }
 
 }
