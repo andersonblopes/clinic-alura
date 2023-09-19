f83ce18468130bd8f7ab56932c679579f9af78d9
Anderson Lopes <>
2023-09-19 19:01:24 +0200

    Configures resource server to validate jwt using public key from authorization server

--- src/main/java/med/lopes/clinic/center/api/security/ResourceServerConfig.java
+++ src/main/java/med/lopes/clinic/center/api/security/ResourceServerConfig.java
@@ -18,7 +18,7 @@ public class ResourceServerConfig {
                 .csrf().disable()
                 .cors()
                 .and()
-                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
 
         return http.build();
     }
