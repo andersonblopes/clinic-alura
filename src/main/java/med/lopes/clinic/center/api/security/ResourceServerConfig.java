9ab30aac8dc0fe150b1d1b45657b8efbcbd0e001
Anderson Lopes <>
2023-09-30 20:48:55 +0200

    feat: Improves security

--- src/main/java/med/lopes/clinic/center/api/security/ResourceServerConfig.java
+++ src/main/java/med/lopes/clinic/center/api/security/ResourceServerConfig.java
@@ -6,21 +6,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class ResourceServerConfig {
 
     @Bean
     public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/oauth2/**").authenticated()
-                .and()
-                .csrf().disable()
-                .cors()
-                .and()
-                .oauth2ResourceServer().jwt();
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
 
         return http.build();
+
     }
 
 }
